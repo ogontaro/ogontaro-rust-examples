500540af8ce3e2f9246bdb2a76590309770283a5
ogontaro <ogontaro@gmail.com>
2023-02-05 13:54:26 +0900

    update

--- examples/vector/src/main.rs
+++ examples/vector/src/main.rs
@@ -1,1 +1,33 @@
-fn main() {}
+struct Pokemon {
+    name: String,
+    level: u8,
+}
+
+fn main() {
+    let pokemons = vec![
+        Pokemon {
+            name: "Pikachu".to_string(),
+            level: 5,
+        },
+        Pokemon {
+            name: "Bulbasaur".to_string(),
+            level: 10,
+        },
+        Pokemon {
+            name: "Charmander".to_string(),
+            level: 15,
+        },
+        Pokemon {
+            name: "Squirtle".to_string(),
+            level: 20,
+        },
+    ];
+    let low_level_pokemons = pokemons.iter().filter(|p| p.level < 15);
+    for pokemon in low_level_pokemons {
+        println!("{} is a low level pokemon", pokemon.name);
+    }
+    let high_level_pokemons = pokemons.iter().filter(|p| p.level >= 15);
+    for pokemon in high_level_pokemons {
+        println!("{} is a high level pokemon", pokemon.name);
+    }
+}
